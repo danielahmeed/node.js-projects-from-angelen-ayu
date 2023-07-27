35e31b8737bb0995933d1644ac9520e49d046ebf
Daniel Ahmed <115207450+danielahmeed@users.noreply.github.com>
2023-07-27 11:09:12 +0530

    Add files via upload

---
+++ 2.3 NPM/tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+const superheroes = require('superheroes');
+ 
+superheroes.all;
+//=> ['3-D Man', 'A-Bomb', …]
+ 
+superheroes.random();
+//=> 'Spider-Ham'
+
+console.log(`My name is ${sillyName}.`);
